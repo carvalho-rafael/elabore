647bf0fa84b39ed02945115dceebf9626d80639e
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2019-03-10 22:10:02 -0300

    homepage structure done

--- resources/views/Site/index.blade.php
+++ resources/views/Site/index.blade.php
@@ -2,14 +2,46 @@
 <html lang="pt-br">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Rafael Carvalho">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Elabore</title>
+    <meta name="author" content="Rafael Carvalho">
+    <meta name="description" content="A Elabore é uma empresa de ensino multidisciplinar que oferece cursos como 
+    pré-vestibular e reforço escolar. conta com uma equipe de professores">
+    <link rel="shortcut icon" type="image/png" href="images/icon.png" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
-    @stack('style');
+    @stack('style')
 </head>
 <body>
-    Página inicial
+    <nav id="main-menu">
+        <ul class="menu-container">
+            <li class="menu-item"><a href="#about">Sobre Nós</a></li>
+            <li class="menu-item"><a href="#team">Nossa Equipe</a></li>
+            <li class="menu-item"><a href="#gallery">Galeria</a></li>
+            <li class="menu-item"><a href="#blog">Blog</a></li>
+            <li class="menu-item"><a href="#contact">Contato</a></li>
+        </ul>
+    </nav>
+    <header id="main-header">MY HEADER</header>
+    <section id="about" class="section">
+        <h1>Quem somos?</h1>
+        <div class="section-content"></div>
+    </section>
+    <section id="team" class="section">
+        <h1>Nossa Equipe</h1>
+        <div class="section-content"></div>
+    </section>
+    <section id="gallery" class="section">
+        <h1>Conheça mais nosso trabalho</h1>
+        <div class="section-content"></div>
+    </section>
+    <section id="blog" class="section">
+        <h1>Blog da Elabore</h1>
+        <div class="section-content"></div>
+    </section>
+    <section id="contact" class="section">
+        <h1>Contato</h1>
+        <div class="section-content"></div>
+    </section>
 </body>
 </html>
