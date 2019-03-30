43810adb6c9df69ec7c96f3fda60ed7e2d37cef3
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2019-03-30 02:35:25 -0300

    change PNGs to svg, set Site Colors

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+
+const goTop = document.getElementById("go-top");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 900){
+        goTop.style.visibility = "visible";
+    }else {
+        goTop.style.visibility = "hidden";
+    }
+}
+
+goTop.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+document.querySelectorAll('.menu-item').forEach(function(element){
+    element.onclick = function(e){
+        e.preventDefault();
+        topSection = document.querySelector(element.getAttribute('href')).offsetTop;
+        window.scrollTo({ 
+            top: topSection - 100,
+            behavior: 'smooth' 
+        });
+    };
+});
--- resources/views/Site/index.blade.php
+++ resources/views/Site/index.blade.php
@@ -15,47 +15,66 @@
 <body>
     <nav id="main-menu">
         <div class="logo-container">
-            <img class="logo" src="images/logo.png" alt="Elabore">
+            <img class="logo" src="images/logo.svg" alt="Elabore">
         </div>
         <ul class="menu-container">
-            <li class="menu-item"><a href="#about">Sobre Nós</a></li>
-            <li class="menu-item"><a href="#team"> Nossa Equipe</a></li>
-            <li class="menu-item"><a href="#gallery">Galeria</a></li>
-            <li class="menu-item"><a href="#blog">Blog</a></li>
-            <li class="menu-item"><a href="#contact">Contato</a></li>
+            <li><a class="menu-item" href="#about">Sobre Nós</a></li>
+            <li><a class="menu-item" href="#team"> Nossa Equipe</a></li>
+            <li><a class="menu-item" href="#gallery">Galeria</a></li>
+            <li><a class="menu-item" href="#blog">Blog</a></li>
+            <li><a class="menu-item" href="#contact">Contato</a></li>
         </ul>
-        <svg preserveAspectRatio="none" version="1.1" viewBox="0 0 94.434 10.597" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <g transform="translate(-6.82 -123.37)">
-                <path id="nav-effect" d="m6.82 123.37h94.434s-28.574 5.5937-46.094 7.2846c-38.404 3.7064-48.34 3.2987-48.34 3.2987z"/>
-            </g>
-        </svg>
-        <svg style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
-            <linearGradient id="my-cool-gradient">
-                <stop offset="0%" stop-color="#00e1eb" />
-                <stop offset="100%" stop-color="#ac30ba" />
-            </linearGradient>
-        </svg>
+        <!-- <div id="nav-effect"></div> -->
     </nav>
-    <header id="main-header"><p class="header-title">Boas ideias <br>saem do papel...</p></header>
+    <header id="main-header">
+        <img id="cover-cloud2" src="images/cloud2.svg" alt="">
+        <img id="cover-boy" src="images/boy.svg" alt="">
+        <img id="cover-cloud1" src="images/cloud1.svg" alt="">
+        <p class="header-title">Boas ideias <br>saem do papel...</p>
+        <a href="#about" class="scroll-down menu-item"></a>
+    </header>
     <section id="about" class="section">
-        <h1>A Elabore</h1>
-        <div class="section-content"></div>
+        <h1>A ELABORE</h1>
+        <div class="section-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nisl ipsum, blandit at vulputate vitae, suscipit vel erat. Nam eu viverra nisi. Aenean rhoncus maximus nunc, sed efficitur ligula viverra congue. Nulla maximus enim ut risus dignissim egestas. Curabitur quis venenatis ante, vel tincidunt nibh.</p>
+        </div>
     </section>
     <section id="team" class="section">
         <h1>Nossa Equipe</h1>
-        <div class="section-content"></div>
+        <img src="images/team.jpg" height="400px" width="auto" alt="">
+        <div class="section-content">
+            <p>Somos uma equipe de profissionais capacitados e que amam o que fazem.</p>
+            <p>Nos dedicados o máximo para que nossos alunos obtenham conhecimento e consiga os melhores resultados.</p>
+        </div>
     </section>
     <section id="gallery" class="section">
         <h1>Conheça mais nosso trabalho</h1>
-        <div class="section-content"></div>
+        <div class="section-content">
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        </div>
     </section>
     <section id="blog" class="section">
         <h1>Blog da Elabore</h1>
+        <div class="section-content"></div>
+    </section>
+    <section id="quote" class="section">
         <div class="section-content"></div>
     </section>
     <section id="contact" class="section">
         <h1>Contato</h1>
         <div class="section-content"></div>
     </section>
+    <div id="go-top">
+    <svg id="go-top-arrow" width="43.941mm" height="31.618mm" version="1.1" viewBox="0 0 43.941 31.618" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(-47.276 -38.089)">
+            <path transform="scale(.26458)" d="m261.33 162.86-67.596 84.93 0.7832 0.61914 67.205-84.436 67.203 84.436 0.7832-0.61914-67.596-84.93-0.39063 0.31055z" color="#000000" color-rendering="auto" dominant-baseline="auto" fill="#fff" image-rendering="auto" shape-rendering="auto" solid-color="#000000" stroke="#fff" stroke-dashoffset="86.213" stroke-linecap="round" stroke-linejoin="round" stroke-width="30.104" style="font-feature-settings:normal;font-variant-alternates:normal;font-variant-caps:normal;font-variant-ligatures:normal;font-variant-numeric:normal;font-variant-position:normal;isolation:auto;mix-blend-mode:normal;paint-order:normal;shape-padding:0;text-decoration-color:#000000;text-decoration-line:none;text-decoration-style:solid;text-indent:0;text-orientation:mixed;text-transform:none;white-space:normal"/>
+        </g>
+    </svg>
+
+    </div>
+
+    <script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
